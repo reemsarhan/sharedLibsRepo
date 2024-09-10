5298b4858f9112823743ec4ca88c3e3d2bfd29e0
reemsarhan <115352161+reemsarhan@users.noreply.github.com>
2024-09-10 19:29:03 +0300

    Update dockerOperations.groovy

--- vars/dockerOperations.groovy
+++ vars/dockerOperations.groovy
@@ -1,6 +1,6 @@
 // vars/dockerOperations.groovy
 
- def call(String repoDir, String imageTag) {
+ def BuildAndPush(String repoDir, String imageTag) {
     dir(repoDir) {
         if (isUnix()) {
             sh "docker build -t reemwaleed/new-deploymentnew-image:${imageTag} ."
@@ -13,3 +13,36 @@
         }
     }
 }
+
+def generateDeploymentYAML(imageTag, filePath) {
+    sh """
+    cat <<EOF > ${filePath}/deployment.yaml
+    apiVersion: apps/v1
+    kind: Deployment
+    metadata:
+      name: library-json-deployment
+    spec:
+      replicas: 1
+      selector:
+        matchLabels:
+          app: library-json
+      template:
+        metadata:
+          labels:
+            app: library-json
+        spec:
+          containers:
+            
+name: library-json-container
+            image: reemwaleed/new-deployment-image:${imageTag}
+            resources:
+              limits:
+                memory: "512Mi"
+                cpu: "500m"
+              requests:
+                memory: "256Mi"
+                cpu: "250m"
+  EOF"""
+}
+
+
