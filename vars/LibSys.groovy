29dbc1f7731cd79ecaad135187e58d7ee2e30dfe
reemsarhan <115352161+reemsarhan@users.noreply.github.com>
2024-09-10 10:35:18 +0300

    Create LibSys.groovy
    Build and Push Docker images

---
+++ vars/LibSys.groovy
@@ -0,0 +1,13 @@
+def buildAndPushDockerImage(String repoDir, String imageTag) {
+    dir(repoDir) {
+        if (isUnix()) {
+            sh "docker build -t reemwaleed/new-deploymentnew-image:${imageTag} ."
+            sh "docker push reemwaleed/new-deploymentnew-image:${imageTag}"
+            sh "docker rmi -f reemwaleed/new-deploymentnew-image:${imageTag}"
+        } else {
+            bat "docker build -t reemwaleed/new-deploymentnew-image:${imageTag} ."
+            bat "docker push reemwaleed/new-deploymentnew-image:${imageTag}"
+            bat "docker rmi -f reemwaleed/new-deploymentnew-image:${imageTag}"
+        }
+    }
+}
